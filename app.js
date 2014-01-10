32bef7340d60d16a79a78f9d09d6400f235b1785
George <georgandco@gmail.com>
2014-01-10 11:13:10 +0200

    09.01.2014

--- app.js
+++ app.js
@@ -12,13 +12,21 @@ app.configure(function(){
 	app.set('view engine', 'dust');
 	app.use(express.favicon());
 	app.use(express.logger('dev'));
+	app.use(express.staticCache({ maxObjects: 100, maxLength: 512 }));
 	app.use(express.static(__dirname + '/public'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
+	app.use(express.directory(__dirname + '/public'));
+	app.use(function(req, res, next) {
+		throw new Error(req.url + ' not found');
+	});
+	app.use(function(err, req, res, next) {
+		res.send(err.message);
+	});
 });
 app.configure('development', function() {
-	app.use(express.errorHandler());
+	app.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
 });
 app.get('/', routes.index);
 http.createServer(app).listen(3000);
